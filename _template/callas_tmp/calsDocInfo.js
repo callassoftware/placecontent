3d7271f11ca0967db39b740e18027c008b32a5be
David van Driessche <david.van.driessche@fourpees.com>
2017-10-05 13:50:28 +0200

    Complete template (with exception of scripts)

---
+++ _template/callas_tmp/calsDocInfo.js
@@ -0,0 +1,32 @@
+var cals_doc_info = 
+{
+	'document':
+	{
+		'name': 'DummyPDF+Boxen - test.pdf'
+		,'path': '\/Users\/dvd\/Desktop'
+		,'info':
+			{
+				'author': null
+				,'title': null
+				,'subject': null
+				,'keywords': null
+			}
+		,'numberofpages': 1
+		,'completeinkinformation': false
+		,'variables':
+			[
+			]
+	}
+	,'pages': 
+		[
+			{
+				'mediabox': [-42.5197,-42.5197,680.316,926.93]
+				,'trimbox': [0,0,595.276,841.89]
+				,'bleedbox': [-14.1732,-14.1732,623.622,870.236]
+				,'cropbox': [-42.5197,-42.5197,680.316,926.93]
+				,'artbox': null
+				,'pagenumber': 0
+				,'pagelabel': null
+			}
+		]
+};
---
+++ _template/scripts/script.js
@@ -0,0 +1,64 @@
+//-------------------------------------------------------------------------------------------------
+// Main Javascript for the Place Content fixup template
+//
+//-------------------------------------------------------------------------------------------------
+// Copyright © 2017 - Four Pees
+// Author: David van Driessche
+//-------------------------------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------------------------------
+// cchipPrintLoop
+//
+// If present in a Javascript file referenced from the template HTML; pdfToolbox will call this
+// function (the function with the exact name "cchipPrintLoop") automagically. If it is present,
+// this function is responsible for outputting the PDF document that will be overlaid on the PDF
+// document that is being processed.
+//
+// This function can refer to the cals_doc_info object to figure out information about the document
+// being processed (such as the number of pages) and can use the cchip.printPages() function to
+// generate PDF from the current state of the HTML template.
+//-------------------------------------------------------------------------------------------------
+
+function cchipPrintLoop() {
+
+	// If you want to add dynamic HTML content to the document, you can get the "body" element. New
+	// content can then be added to the body element
+	var theBody = $( "body" );
+
+	// Loop over all pages
+	for( var thePageIndex = 0; thePageIndex < cals_doc_info.pages.length; thePageIndex++ ) {
+
+
+
+		// Output to the current page
+		cchip.printPages(1);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
