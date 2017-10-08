12e48285512306fdf0ac5646a973482ed52403bd
David van Driessche <david.van.driessche@fourpees.com>
2017-10-08 14:37:16 +0200

    Truncating support in string conversion

--- _template/scripts/cals-units.js
+++ _template/scripts/cals-units.js
@@ -18,13 +18,13 @@
 // Example use: unitUnits.mm
 var unitUnits = {
 
-	point: 			{ value: 1, ptFactor: 1,			pxFactor: 1.3333333333333 },
-	mm: 			{ value: 2, ptFactor: 2.83465,		pxFactor: 3.7795275590551 },
-	cm: 			{ value: 3, ptFactor: 28.3465,		pxFactor: 37.795275590551 },
-	m: 				{ value: 4, ptFactor: 2834.65,		pxFactor: 3779.5275590551 },
-	inch: 			{ value: 5, ptFactor: 72,			pxFactor: 96 },
-	foot: 			{ value: 6, ptFactor: 864,			pxFactor: 1152 },
-	px: 			{ value: 7, ptFactor: 0.75,			pxFactor: 1 }
+    point:          { value: 1, ptFactor: 1,            pxFactor: 1.3333333333333 },
+    mm:             { value: 2, ptFactor: 2.83465,      pxFactor: 3.7795275590551 },
+    cm:             { value: 3, ptFactor: 28.3465,      pxFactor: 37.795275590551 },
+    m:              { value: 4, ptFactor: 2834.65,      pxFactor: 3779.5275590551 },
+    inch:           { value: 5, ptFactor: 72,           pxFactor: 96 },
+    foot:           { value: 6, ptFactor: 864,          pxFactor: 1152 },
+    px:             { value: 7, ptFactor: 0.75,         pxFactor: 1 }
 };
 Object.freeze(unitUnits);
 
@@ -38,7 +38,7 @@ Object.freeze(unitUnits);
 //
 function unitConvertToPoints( inValue, inUnit ) {
 
-	return inValue * inUnit.ptFactor;
+    return inValue * inUnit.ptFactor;
 }
 
 // Converts the incoming value in points to the given unit
@@ -46,7 +46,7 @@ function unitConvertToPoints( inValue, inUnit ) {
 //
 function unitConvertFromPoints( inValue, inUnit ) {
 
-	return inValue / inUnit.ptFactor;
+    return inValue / inUnit.ptFactor;
 }
 
 // Converts the incoming value (in the given unit) to pixels
@@ -54,7 +54,7 @@ function unitConvertFromPoints( inValue, inUnit ) {
 //
 function unitConvertToPixels( inValue, inUnit ) {
 
-	return inValue * inUnit.pxFactor;
+    return inValue * inUnit.pxFactor;
 }
 
 // Converts the incoming value in pixels to the given unit
@@ -62,7 +62,7 @@ function unitConvertToPixels( inValue, inUnit ) {
 //
 function unitConvertFromPixels( inValue, inUnit ) {
 
-	return inValue / inUnit.pxFactor;
+    return inValue / inUnit.pxFactor;
 }
 
 // Convenience routine to convert points to pixels
@@ -70,7 +70,7 @@ function unitConvertFromPixels( inValue, inUnit ) {
 //
 function unitPoints2Pixels( inPoints ) {
 
-	return unitConvertToPixels( inPoints, unitUnits.point );
+    return unitConvertToPixels( inPoints, unitUnits.point );
 }
 
 // Convenience routine to convert pixels to points
@@ -78,7 +78,7 @@ function unitPoints2Pixels( inPoints ) {
 //
 function unitPixels2Point( inPixels ) {
 
-	return unitConvertToPoints( inPixels, unitUnits.px );
+    return unitConvertToPoints( inPixels, unitUnits.px );
 }
 
 // Convenience routine to convert one unit into another unit
@@ -86,8 +86,8 @@ function unitPixels2Point( inPixels ) {
 //
 function unitConvertToFrom( inValue, inFromUnit, inToUnit ) {
 
-	var thePoints = unitConvertToPoints( inValue, inFromUnit );
-	return unitConvertFromPoints( thePoints, inToUnit );
+    var thePoints = unitConvertToPoints( inValue, inFromUnit );
+    return unitConvertFromPoints( thePoints, inToUnit );
 }
 
 
@@ -99,42 +99,42 @@ function unitConvertToFrom( inValue, inFromUnit, inToUnit ) {
 // Converts the given value and measurement unit into a nicely formatted string
 // Example: var theString = unitFormatWithUnit( 100, unitUnits.px );
 //
-function unitFormatWithUnit( inValue, inUnit ) {
+function unitFormatWithUnit( inValue, inUnit, inNumDigits ) {
     
     switch( inUnit ) {
             
         case unitUnits.point: {
-            return inValue + " pt";
+            return inValue.toFixed( inNumDigits ) + " pt";
         }   
             
         case unitUnits.px: {
-            return inValue + " px";
+            return inValue.toFixed( inNumDigits ) + " px";
         }   
             
         case unitUnits.mm: {
-            return inValue + " mm";
+            return inValue.toFixed( inNumDigits ) + " mm";
         }   
             
         case unitUnits.cm: {
-            return inValue + " cm";
+            return inValue.toFixed( inNumDigits ) + " cm";
         }   
             
         case unitUnits.m: {
-            return inValue + " m";
+            return inValue.toFixed( inNumDigits ) + " m";
         }   
             
         case unitUnits.inch: {
             var theInch = inValue % 12;
-            var theFeet = (inValue - theInch) / 12;
-            return theFeet + "' " + theInch + "\"";
+            var theFeet = inValue / 12;
+            return theFeet.toFixed(0) + "' " + theInch.toFixed( inNumDigits ) + "\"";
         }   
         
         case unitUnits.foot: {
-            return inValue + "'";
+            return inValue.toFixed( inNumDigits ) + "'";
         }   
             
         default: {
-            return inValue + " penguins";
+            return inValue.toFixed( inNumDigits ) + " penguins";
         }       
     }
 }
