0e6566bd76dea3a35746517ae55f863ad7f27415
David van Driessche <david.van.driessche@fourpees.com>
2017-10-08 18:07:21 +0200

    Add “Add dimension arrows” example

---
+++ Add dimension arrows (SVG)/callas_tmp/calsDocInfo.js
@@ -0,0 +1,32 @@
+var cals_doc_info = 
+{
+	'document':
+	{
+		'name': '210x285_ClassicCar_Page - test.pdf'
+		,'path': '\/Users\/dvd\/Desktop'
+		,'info':
+			{
+				'author': 'WolfgangK'
+				,'title': '210x285_ClassicCar_Pagode_e'
+				,'subject': null
+				,'keywords': null
+			}
+		,'numberofpages': 1
+		,'completeinkinformation': false
+		,'variables':
+			[
+			]
+	}
+	,'pages': 
+		[
+			{
+				'mediabox': [0,0,651,958]
+				,'trimbox': [28.3465,120.472,594.307,809.182]
+				,'bleedbox': null
+				,'cropbox': [0,0,651,958]
+				,'artbox': null
+				,'pagenumber': 0
+				,'pagelabel': '1'
+			}
+		]
+};
